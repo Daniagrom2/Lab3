a8d9403fbc867ba175e5d4abf35e346efabb45d3
Daniagrom2 <daniagrom5@gmail.com>
2021-12-16 15:51:34 +0200

    lab3

---
+++ KrossLab_3/KrossLab_3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ClassLibrary1;
+namespace KrossLab_3
+{
+    
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Computer a = new Computer("Asus",16,"Nvidia GTX 1050Ti","Intel Core i5");
+            Console.Write($"{a.Name} {a.GPU_Name} {a.CPU_Name} {a.RAM_Count}");
+        }
+    }
+}
